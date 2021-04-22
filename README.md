fca2045abac33c5bced49b5dd55dc5ea7baa9b27
Roberto Kok <info@rkok.nl>
2021-04-22 18:52:26 +0200

    Run prettier

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # CodeMirror Spell Checker
+
 Spell checking so simple, you can set up in 60 seconds. It will highlight any misspelled words in light red. Works great in conjunction with other CodeMirror modes, like GitHub Flavored Markdown.
 
 [Demo](http://nextstepwebs.github.io/codemirror-spell-checker/)
@@ -8,16 +9,18 @@ Spell checking so simple, you can set up in 60 seconds. It will highlight any mi
 ## Install
 
 Via [npm](https://www.npmjs.com/package/codemirror-spell-checker).
+
 ```
 npm install codemirror-spell-checker --save
 ```
 
 Via [bower](https://www.bower.io).
+
 ```
 bower install codemirror-spell-checker --save
 ```
 
-Via [jsDelivr](https://www.jsdelivr.com/projects/codemirror.spell-checker). *Please note, jsDelivr may take a few days to update to the latest release.*
+Via [jsDelivr](https://www.jsdelivr.com/projects/codemirror.spell-checker). _Please note, jsDelivr may take a few days to update to the latest release._
 
 ```HTML
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/codemirror.spell-checker/latest/spell-checker.min.css">
@@ -25,6 +28,7 @@ Via [jsDelivr](https://www.jsdelivr.com/projects/codemirror.spell-checker). *Ple
 ```
 
 ## Quick start
+
 Once CodeMirror is installed and loaded, first provide CodeMirror Spell Checker with the correct CodeMirror function. Then, just set the primary mode to `"spell-checker"` and the backdrop mode to your desired mode. Be sure to load/require `overlay.min.js` if you haven't already.
 
 ```JS
@@ -41,6 +45,7 @@ CodeMirror.fromTextArea(document.getElementById("textarea"), {
 That's it!
 
 ## Customizing
+
 You can customize the misspelled word appearance by updating the CSS. All misspelled words will have the `.cm-spell-error` class.
 
 ```CSS
--- gulpfile.js
+++ gulpfile.js
@@ -1,81 +1,92 @@
 "use strict";
 
 var gulp = require("gulp"),
-	minifycss = require("gulp-clean-css"),
-	uglify = require("gulp-uglify"),
-	concat = require("gulp-concat"),
-	header = require("gulp-header"),
-	buffer = require("vinyl-buffer"),
-	pkg = require("./package.json"),
-	debug = require("gulp-debug"),
-	eslint = require("gulp-eslint"),
-	browserify = require("browserify"),
-	source = require("vinyl-source-stream"),
-	rename = require("gulp-rename");
+  minifycss = require("gulp-clean-css"),
+  uglify = require("gulp-uglify"),
+  concat = require("gulp-concat"),
+  header = require("gulp-header"),
+  buffer = require("vinyl-buffer"),
+  pkg = require("./package.json"),
+  debug = require("gulp-debug"),
+  eslint = require("gulp-eslint"),
+  browserify = require("browserify"),
+  source = require("vinyl-source-stream"),
+  rename = require("gulp-rename");
 
-var banner = ["/**",
-	" * <%= pkg.name %> v<%= pkg.version %>",
-	" * Copyright <%= pkg.company %>",
-	" * @link <%= pkg.homepage %>",
-	" * @license <%= pkg.license %>",
-	" */",
-	""].join("\n");
+var banner = [
+  "/**",
+  " * <%= pkg.name %> v<%= pkg.version %>",
+  " * Copyright <%= pkg.company %>",
+  " * @link <%= pkg.homepage %>",
+  " * @license <%= pkg.license %>",
+  " */",
+  "",
+].join("\n");
 
-gulp.task("lint", function() {
-	return gulp.src("./src/js/**/*.js")
-		.pipe(debug())
-		.pipe(eslint())
-		.pipe(eslint.format())
-		.pipe(eslint.failAfterError());
+gulp.task("lint", function () {
+  return gulp
+    .src("./src/js/**/*.js")
+    .pipe(debug())
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
 function taskBrowserify(opts) {
-	return browserify("./src/js/spell-checker.js", opts)
-		.bundle();
+  return browserify("./src/js/spell-checker.js", opts).bundle();
 }
 
-gulp.task("browserify:debug", gulp.series("lint", function() {
-	return taskBrowserify({debug:true, standalone:"CodeMirrorSpellChecker"})
-		.pipe(source("spell-checker.debug.js"))
-		.pipe(buffer())
-		.pipe(header(banner, {pkg: pkg}))
-		.pipe(gulp.dest("./debug/"));
-}));
+gulp.task(
+  "browserify:debug",
+  gulp.series("lint", function () {
+    return taskBrowserify({ debug: true, standalone: "CodeMirrorSpellChecker" })
+      .pipe(source("spell-checker.debug.js"))
+      .pipe(buffer())
+      .pipe(header(banner, { pkg: pkg }))
+      .pipe(gulp.dest("./debug/"));
+  })
+);
 
-gulp.task("browserify", gulp.series("lint", function() {
-	return taskBrowserify({standalone:"CodeMirrorSpellChecker"})
-		.pipe(source("spell-checker.js"))
-		.pipe(buffer())
-		.pipe(header(banner, {pkg: pkg}))
-		.pipe(gulp.dest("./debug/"));
-}));
+gulp.task(
+  "browserify",
+  gulp.series("lint", function () {
+    return taskBrowserify({ standalone: "CodeMirrorSpellChecker" })
+      .pipe(source("spell-checker.js"))
+      .pipe(buffer())
+      .pipe(header(banner, { pkg: pkg }))
+      .pipe(gulp.dest("./debug/"));
+  })
+);
 
-gulp.task("scripts", gulp.series("browserify:debug", "browserify", "lint", function() {
-	var js_files = ["./debug/spell-checker.js"];
-	
-	return gulp.src(js_files)
-		.pipe(concat("spell-checker.min.js"))
-		.pipe(uglify())
-		.pipe(buffer())
-		.pipe(header(banner, {pkg: pkg}))
-		.pipe(gulp.dest("./dist/"));
-}));
+gulp.task(
+  "scripts",
+  gulp.series("browserify:debug", "browserify", "lint", function () {
+    var js_files = ["./debug/spell-checker.js"];
 
-gulp.task("styles", function() {
-	var css_files = [
-		"./src/css/*.css",
-	];
-	
-	return gulp.src(css_files)
-		.pipe(concat("spell-checker.css"))
-		.pipe(buffer())
-		.pipe(header(banner, {pkg: pkg}))
-		.pipe(gulp.dest("./debug/"))
-		.pipe(minifycss())
-		.pipe(rename("spell-checker.min.css"))
-		.pipe(buffer())
-		.pipe(header(banner, {pkg: pkg}))
-		.pipe(gulp.dest("./dist/"));
+    return gulp
+      .src(js_files)
+      .pipe(concat("spell-checker.min.js"))
+      .pipe(uglify())
+      .pipe(buffer())
+      .pipe(header(banner, { pkg: pkg }))
+      .pipe(gulp.dest("./dist/"));
+  })
+);
+
+gulp.task("styles", function () {
+  var css_files = ["./src/css/*.css"];
+
+  return gulp
+    .src(css_files)
+    .pipe(concat("spell-checker.css"))
+    .pipe(buffer())
+    .pipe(header(banner, { pkg: pkg }))
+    .pipe(gulp.dest("./debug/"))
+    .pipe(minifycss())
+    .pipe(rename("spell-checker.min.css"))
+    .pipe(buffer())
+    .pipe(header(banner, { pkg: pkg }))
+    .pipe(gulp.dest("./dist/"));
 });
 
 gulp.task("default", gulp.series("scripts", "styles"));
